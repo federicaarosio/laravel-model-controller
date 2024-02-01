b1009ece3c4fb0ec4721d070a30721454d54ae6f
federicaarosio <arosio.fede@gmail.com>
2024-02-01 17:46:08 +0100

    added infos in cards

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -2,12 +2,25 @@
 
 @section('main-content')
 
+<div class="container">
+    <div class="row">
 
-    @foreach ($movies as $movie)
-        <article>
-            {{ $movie -> title }}
-        </article>
-    @endforeach
+
+        @foreach ($movies as $movie)
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{ $movie -> title }}</h5>
+              <div class="card-text">Original title: {{ $movie -> original_title }}</div>
+              <div class="card-text">Nationality: {{ $movie -> nationality }}</div>
+              <div class="card-text">Release date: {{ $movie -> date }}</div>
+              <div class="card-text">Vote: {{ $movie -> vote }}</div>
+            </div>
+        </div>
+
+        @endforeach
+    </div>
+</div>
 
 
 @endsection
