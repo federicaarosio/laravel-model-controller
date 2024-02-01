78a419070f087b4f91c988733bed19b745f7a29a
federicaarosio <arosio.fede@gmail.com>
2024-02-01 16:38:48 +0100

    added Movie model

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index() {
-        return 'La mia homepage';
+        return view('movie.index');
     }
 }
---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    qui i movies
+</h1>
