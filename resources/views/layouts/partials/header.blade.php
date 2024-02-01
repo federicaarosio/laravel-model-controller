7164821159c1fb5c8d581cd148e642f39d82cad2
federicaarosio <arosio.fede@gmail.com>
2024-02-01 18:28:06 +0100

    added style

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,2 +1,5 @@
-<header>
+<header class="container text-center my-4">
+    <h1>
+        Movies
+    </h1>
 </header>
--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -5,22 +5,23 @@
 <div class="container">
     <div class="row">
 
-
         @foreach ($movies as $movie)
-
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">{{ $movie -> title }}</h5>
-              <div class="card-text">Original title: {{ $movie -> original_title }}</div>
-              <div class="card-text">Nationality: {{ $movie -> nationality }}</div>
-              <div class="card-text">Release date: {{ $movie -> date }}</div>
-              <div class="card-text">Vote: {{ $movie -> vote }}</div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem; height: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie -> title }}</h5>
+                    <div class="card-text">Original title: {{ $movie -> original_title }}</div>
+                    <div class="card-text">Nationality: {{ $movie -> nationality }}</div>
+                    <div class="card-text">Release date: {{ $movie -> date }}</div>
+                    <div class="card-text">Vote: {{ $movie -> vote }}</div>
+                </div>
             </div>
         </div>
 
         @endforeach
     </div>
 </div>
+
 
 
 @endsection
